8be15ab332fa46a060dcd292e17785398ee4fcda
AndrewDongminYoo <ydm2790@gmail.com>
2021-09-17 22:49:27 +0900

    quiz done!!!

--- test/app.py
+++ test/app.py
@@ -1,3 +1,5 @@
+import json
+
 from dbget import get_stock_info
 from pymongo import MongoClient
 import jwt
@@ -7,19 +9,33 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 # from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
-# client = MongoClient('내 AWS 아이피', 27017, username="아이디", password="비밀번호")
-db = client.dbsparta_plus
+client = MongoClient("mongodb://localhost:27017/")
+db = client.dbStock
+
+
+@app.route('/')
+def home():
+    return render_template('stock.html')
+
 
 # 백엔드
 # 생략은 코드가 생략되었다는...
-@app.route('/stock', methods = ['POST'])
+@app.route('/stock', methods=['POST'])
 def save_info():
+    print(request.form)
     market = request.form.get('market')
     sector = request.form.get('sector')
     tag = request.form.get('tag')
-    result = db.strocks.find({"sector": sector, "market": market, "tag": tag}, {'_id': False})
+    query = {"sector": "sector-"+sector, "market": "market-"+market, "tag": "tag-"+tag}
+    result = list(db.stocks.find(query, {'_id': False}))
     return jsonify({'message': '결과가 나왔습니다!', 'result': result})
+
+
+@app.route('/company', methods=["GET"])
+def search():
+    code = request.form.get('code')
+    [m, n, o] = get_stock_info(code)
+    return jsonify({"message": "회사 정보", "result": {"m": m, "n": n, "o": o}})
 
 
 if __name__ == '__main__':
--- test/dbget.py
+++ test/dbget.py
@@ -6,13 +6,13 @@ driver = Chrome(options=options)
 driver.implicitly_wait(3)
 
 
-def get_stock_info(code: str) -> tuple:
+def get_stock_info(code: str) -> list:
     url = f"https://finance.naver.com/item/main.nhn?code={code}"
     driver.get(url)
     price = driver.find_element_by_css_selector('#chart_area > div.rate_info > div > p.no_today').text.join('')
     si_total = driver.find_element_by_css_selector('#tab_con1 > div.first > table > tbody > tr.strong > td').text
     per = driver.find_element_by_css_selector('#_per').text
-    return price.strip(), si_total.strip(), per.strip()
+    return [price.strip(), si_total.strip(), per.strip()]
 
 
 if __name__ == '__main__':
