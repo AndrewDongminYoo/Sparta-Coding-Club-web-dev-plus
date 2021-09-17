04bebe27ba8d7d7efd4ff29cfd26b728d70a8552
AndrewDongminYoo <ydm2790@gmail.com>
2021-09-18 05:55:46 +0900

    refactoring: 데이터베이스 쌓는 파이썬 파일을 하나로 합쳤습니다. 파일을 실행할 때마다 새롭게 데이터가 채워집니다. 일부 회사는 데이터가 쓰이지 않을 수도 있습니다.

--- test/dbget.py
+++ test/dbget.py
@@ -1,14 +1,38 @@
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-from dbset import stocks
 from pymongo import MongoClient
-options = Options()
-# options.headless = True
-driver = Chrome(options=options)
+driver = Chrome()
 driver.implicitly_wait(3)
 # 파일을 만들어서 그대로 실행시킵니다!!
 client = MongoClient("mongodb://localhost:27017/")
 db = client.dbStock
+
+db.codes.delete_many({})
+codes = [
+    {"group": "market", "code": "market-1", "name": "코스피"},
+    {"group": "market", "code": "market-2", "name": "코스닥"},
+    {"group": "sector", "code": "sector-1", "name": "반도체와반도체장비"},
+    {"group": "sector", "code": "sector-2", "name": "양방향미디어와서비스"},
+    {"group": "sector", "code": "sector-3", "name": "자동차"},
+    {"group": "tag", "code": "tag-1", "name": "메모리"},
+    {"group": "tag", "code": "tag-2", "name": "플랫폼"},
+    {"group": "tag", "code": "tag-4", "name": "전기차"},
+]
+db.codes.insert_many(codes)
+
+db.stocks.delete_many({})
+stocks = [
+        {"name": "삼성전자", "code": "005930", "sector":"sector-1", "market": "market-1", "tag": "tag-1"},
+        {"name": "SK하이닉스", "code": "000660", "sector":"sector-1", "market": "market-1", "tag": "tag-1"},
+        {"name": "리노공업", "code": "058470", "sector": "sector-1", "market": "market-2", "tag": "tag-1"},
+        {"name": "DB하이텍", "code": "000990", "sector": "sector-1", "market": "market-1", "tag": "tag-1"},
+        {"name": "솔브레인", "code": "357780", "sector": "sector-1", "market": "market-2", "tag": "tag-1"},
+        {"name": "카카오", "code": "357780", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
+        {"name": "네이버", "code": "035420", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
+        {"name": "아프리카TV", "code": "067160", "sector": "sector-2", "market": "market-2", "tag": "tag-3"},
+        {"name": "키다리스튜디오", "code": "020120", "sector": "sector-2", "market": "market-1", "tag": "tag-2"},
+        {"name": "현대차", "code": "005380", "sector": "sector-3", "market": "market-1", "tag": "tag-4"},
+ ]
+db.stocks.insert_many(stocks)
 
 
 def get_stock_info(code: str) -> list:
@@ -18,7 +42,7 @@ def get_stock_info(code: str) -> list:
     si_total = driver.find_element_by_css_selector('#tab_con1 > div.first > table > tbody > tr.strong > td').text
     per = driver.find_element_by_css_selector('#_per').text
     db.stocks.update_one({"code": code}, {"$set": {"price": price, "capitalization": si_total, "PER": per}})
-    return [price.strip(), si_total.strip(), per.strip()]
+    return list(db.find({"code": code}, {"_id": False}))
 
 
 if __name__ == '__main__':
